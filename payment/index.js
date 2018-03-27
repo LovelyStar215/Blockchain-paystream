e2a3cc4bcb981bcf8e8e52da475dbebd5d0d6042
LovelyStar215 <ericjin106@gmail.com>
2018-03-27 14:42:34 +0100

    better api

--- payment/index.js
+++ payment/index.js
@@ -7,9 +7,17 @@ http.createServer(function (req, res) {
     const requestUrl = url.parse(req.url)
 
     if (requestUrl.path === '/GetBalance') {
+        // TODO provide a payment address (shared secret from streaming-shop tutorial)
         res.end('Get balance');
     } else if (requestUrl.path === '/PayFrame') {
+        // TODO provide a payment address (shared secret from streaming-shop tutorial)
         res.end('Pay frame');
+        // returns identifier for payment
+    } else if (requestUrl.path === '/PaymentStatus') {
+        // provide payment identifier
+        res.end('Payment status');
+        // returns status for payment
+        // failure/success/in_progress something like that 
     } else {
         res.statusCode = 404;
         res.end('Page not found')
