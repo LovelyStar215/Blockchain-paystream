01c6e4f9f49f3ffce8328bb4b45a0c42b9435e1f
LovelyStar215 <ericjin106@gmail.com>
2018-03-28 17:25:55 +0200

    test stream payment

--- payment/index.js
+++ payment/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
+const IlpPacket = require('ilp-packet')
 var url = require('url');
 const xrp = require('./interledger').xrp()
+const uuid = require('uuid/v4')
+const crypto = require('crypto')
+
+// https://github.com/interledger/tutorials/blob/master/streaming-payments/streaming-client1.js
+
+function base64url (buf) {
+  return buf.toString('base64')
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+}
+
+function sha256 (preimage) {
+  return crypto.createHash('sha256').update(preimage).digest()
+}
+
+function hmac (secret, input) {
+  return crypto.createHmac('sha256', secret).update(input).digest()
+}
 
 console.log("Starting server..")
 xrp.connect().then(function () {
@@ -9,8 +27,6 @@ xrp.connect().then(function () {
 	var app = express();
 	
 	app.get('/PayFrame', function(req, res){
-		//res.send('id: ' + req.query.id);
-	  
 		var parts = url.parse(req.url, true);
 		var query = parts.query;
 		console.log(query);
@@ -18,11 +34,33 @@ xrp.connect().then(function () {
 		// TODO provide a payment address (shared secret from streaming-shop tutorial)
 		res.end('Pay frame')
 		// returns identifier for payment
+		
+		const destinationAddress = 'test.crypto.xrp.r4SChuqPedRaW1bxhLLvFGpbQYNXEYQY6Y' // todo
+		const destinationAmount = '10' // todo
+		
+		const sharedSecret = Buffer.from('test', 'base64') // todo
+		const ilpPacket = IlpPacket.serializeIlpPayment({
+			account: destinationAddress,
+			amount: destinationAmount,
+			data: ''
+		})
+		const fulfillmentGenerator = hmac(sharedSecret, 'ilp_psk_condition')
+		const fulfillment = hmac(fulfillmentGenerator, ilpPacket)
+		const condition = sha256(fulfillment)
+		
+		xrp.sendTransfer({
+			id: uuid(),
+			from: xrp.getAccount(),
+			to: destinationAddress,
+			ledger: xrp.getInfo().prefix,
+			expiresAt: new Date(new Date().getTime() + 1000000).toISOString(),
+			amount: destinationAmount,
+			executionCondition: base64url(condition),
+			ilp: base64url(ilpPacket)
+		})
 	})
 	
 	app.get('/PaymentStatus', function(req, res){
-		//res.send('id: ' + req.query.id);
-		
 		// provide payment identifier
 		res.end('Payment status');
 		// returns status for payment
